eb57586aebdc43a59c25dd5179fd3bc24e4af76f
493430480@qq.com <xuby0615@163.com>
2016-06-28 10:36:20 +0800

    add cross-domain function in SC_Controller.php

--- system/core/SC_Controller.php
+++ system/core/SC_Controller.php
@@ -6,14 +6,27 @@ class SC_Controller {
 		DI::set('smarty', Factory::smarty());
 	}
 	
+	//返回json格式的数据
 	public function json($array){
 		return json_encode($array);
 	}
+
+	//解决Ajax跨域问题
+	public function domain(){
+		header('Access-Control-Allow-Origin: *');
+		header('Access-Control-Allow-Credentials: true');
+		if('OPTIONS' != $_SERVER['REQUEST_METHOD']) return;
+		header('Access-Control-Allow-Headers: Origin, No-Cache, X-Requested-With, If-Modified-Since, Pragma, Last-Modified, Cache-Control, Expires, Content-Type, X-E4M-With');
+		header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE');
+		header('Access-Control-Max-Age: ' . 3600 * 24);
+	}
 	
+	//邮件配置
 	public function mail(){
 		return DI::get('mail');
 	}
 	
+	//smarty模板
 	public function smarty(){
 		return DI::get('smarty');
 	}
